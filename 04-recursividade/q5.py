1f510e6b8ab0fce61551e506c1aeaa23774f2fba
Pericles Feitoza <periclesanfe@gmail.com>
2023-07-18 19:17:32 -0300

    ex4 q5

--- 04-recursividade/q5.py
+++ 04-recursividade/q5.py
@@ -1,2 +1,13 @@
 # Escreva uma função recursiva que receba um número inteiro positivo N e imprima todos os números naturais de 0 até N em
 # ordem decrescente.
+
+
+def printto(n):
+    if n == 0:
+        print(n)
+    else:
+        str(printto(n - 1))
+        print(n)
+
+
+printto(int(input()))
