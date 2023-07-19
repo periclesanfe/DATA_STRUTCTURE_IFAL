e602351c8e27f357b63791c2bb2d0823fc8ab4c2
Pericles Feitoza <periclesanfe@gmail.com>
2023-07-19 10:29:32 -0300

    ex4 q6

--- 04-recursividade/q6.py
+++ 04-recursividade/q6.py
@@ -1,2 +1,38 @@
 # Escreva uma função recursiva que calcule o Nésimo 10 termo da sequencia de Fibonacci. A sequência de Fibonacci é
 # 0,1, 1, 2, 3, 5, 8, 13, 21,...
+
+
+# def fibonacci(n):
+#    pri = 1
+#    sec = 1
+#    print(pri)
+#    print(sec)
+#    for i in range(0, n - 2):
+#        ter = sec + pri
+#        pri = sec
+#        sec = ter
+#        print(ter)
+
+
+# def fibonacci(n, pri, sec):
+#    if n == 0:
+#        print(sec + pri)
+#    else:
+#        ter = sec + pri
+#        pri = sec
+#        sec = ter
+#        print(ter)
+#        fibonacci(n - 1, pri, sec)
+
+
+def fibonacci(n, pri=1, sec=1):
+    if n == 1:
+        return pri
+    else:
+        return str(pri) + " " + str(fibonacci(n - 1, sec, sec + pri))
+
+
+# TESTE
+print(fibonacci(6))
+print(fibonacci(5))
+print(fibonacci(10))
