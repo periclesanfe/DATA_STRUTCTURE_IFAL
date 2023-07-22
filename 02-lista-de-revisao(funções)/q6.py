65b35aca19d6cdcd3e678f3401bda406aa90f5c6
Pericles Feitoza <106848279+periclesanfe@users.noreply.github.com>
2023-07-22 16:23:54 +0000

    ex2 q6

---
+++ 02-lista-de-revisao(funções)/q6.py
@@ -0,0 +1,12 @@
+# Faça uma função maior_de_3(num1, num2, num3) que, dados três numeros, retorna a maior deles
+
+def maior_de_3(num1, num2, num3):
+    lista = [num1, num2, num3]
+    return max(lista)
+
+
+#testes
+print(maior_de_3(1,2,3))
+print(maior_de_3(10,2,3))
+print(maior_de_3(0,0,0))
+print(maior_de_3(1,25,3))
